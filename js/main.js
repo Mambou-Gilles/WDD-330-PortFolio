3ecd97b744574ef14dc4466aa43b3b31d7485df4
Mambou-Gilles <gillesravelmambou@gmail.com>
2021-09-24 07:28:41 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
 const links = [
-  {
-    label: "Week1 notes",
-    url: "w01/index.html"
-  }
-]
+  { label: 'Week 1 Notes', url: 'week1.html', },
+  { label: 'Week 2 Notes', url: 'week2.html', },
+  { label: 'Week 3 Notes', url: 'week3.html', },
+];
+
+const ol = document.getElementById('assignments');   
+links.forEach(link => {
+  let li = document.createElement('li');
+  let a = document.createElement('a');
+  a.setAttribute('href', link.url);
+  a.innerText = link.label;
+ 
+  li.appendChild(a);
+  ol.appendChild(li);
+});
