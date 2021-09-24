8ba76d799e9f597d6e822733f5586b75c9fe45cf
Mambou-Gilles <gillesravelmambou@gmail.com>
2021-09-24 07:47:30 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 const links = [
-  { label: 'Week 1 Notes', url: 'w01/index.html' }
+  { label: 'Week 1 Notes', url: 'w01/index.html' },
+  { label: 'Week 2 Notes', url: 'week2.html' },
+  { label: 'Week 3 Notes', url: 'week3.html' },
+
 ];
 
 const ol = document.getElementById('assignments');   
