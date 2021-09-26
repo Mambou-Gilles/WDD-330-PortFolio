9da749f678ee3d1a7f47df308a248ca0accce0e9
Mambou-Gilles <gillesravelmambou@gmail.com>
2021-09-26 07:30:29 +0200

    Update main.js

--- w01/scripts/main.js
+++ w01/scripts/main.js
@@ -9,3 +9,6 @@
 //const currentDate = new Date();
 //document.querySelector('#year').textContent = currentDate.getFullYear();
 document.getElementById('year').innerHTML = (new Date).getFullYear();
+const d = new Date();
+d.setDate(d.getDate() + 0);
+document.getElementById("update").innerHTML = "Last Updated: " + " " + d;
